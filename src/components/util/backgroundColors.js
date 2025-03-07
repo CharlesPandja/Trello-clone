ac8662eff050b51819eaba8ba22fc0a6ba0a0363
CharlesPandja <pandjacharles@gmail.com>
2025-03-07 17:10:14 +0100

    Adding gradient to backgrounds colors of tableaux

--- src/components/util/backgroundColors.js
+++ src/components/util/backgroundColors.js
@@ -1,12 +1,25 @@
+// export const backgroundColors = [
+//     { id: 0, color: 'bg-blue-500', name: 'bleue'},
+//     { id: 1, color: 'bg-red-500', name: 'rouge'},
+//     { id: 2, color: 'bg-stone-500', name: 'grise foncée'},
+//     { id: 3, color: 'bg-purple-400', name: 'violet léger'},
+//     { id: 4, color: 'bg-fuchsia-400', name: 'violet pastel'},
+//     { id: 5, color: 'bg-neutral-600', name: 'sombre clair'},
+//     { id: 6, color: 'bg-slate-800', name: 'bleue foncé'},
+//     { id: 7, color: 'bg-purple-900', name: 'violet foncé'},
+//     { id: 8, color: 'bg-pink-950', name: 'marron'},
+//     { id: 9, color: 'bg-stone-200', name: 'gris'},
+// ]
+
 export const backgroundColors = [
-    { id: 0, color: 'bg-blue-500', name: 'bleue'},
-    { id: 1, color: 'bg-red-500', name: 'rouge'},
-    { id: 2, color: 'bg-stone-500', name: 'grise foncée'},
-    { id: 3, color: 'bg-purple-400', name: 'violet léger'},
-    { id: 4, color: 'bg-fuchsia-400', name: 'violet pastel'},
-    { id: 5, color: 'bg-neutral-600', name: 'sombre clair'},
-    { id: 6, color: 'bg-slate-800', name: 'bleue foncé'},
-    { id: 7, color: 'bg-purple-900', name: 'violet foncé'},
-    { id: 8, color: 'bg-pink-950', name: 'marron'},
-    { id: 9, color: 'bg-stone-200', name: 'gris'},
-]
+    { id: 0, color: 'bg-gradient-to-b from-blue-500 to-indigo-800', name: 'bleu dégradé' },
+    { id: 1, color: 'bg-gradient-to-b from-red-500 to-pink-500', name: 'rouge rosé' },
+    { id: 2, color: 'bg-gradient-to-b from-gray-600 to-gray-800', name: 'gris foncé' },
+    { id: 3, color: 'bg-gradient-to-b from-purple-400 to-violet-500', name: 'violet léger' },
+    { id: 4, color: 'bg-gradient-to-b from-fuchsia-400 to-pink-500', name: 'rose fuchsia' },
+    { id: 5, color: 'bg-gradient-to-b from-neutral-500 to-neutral-700', name: 'sombre clair' },
+    { id: 6, color: 'bg-gradient-to-b from-blue-800 to-slate-900', name: 'bleu nuit' },
+    { id: 7, color: 'bg-gradient-to-b from-purple-900 to-indigo-900', name: 'violet profond' },
+    { id: 8, color: 'bg-gradient-to-b from-amber-900 to-orange-700', name: 'brun doré' },
+    { id: 9, color: 'bg-gradient-to-b from-stone-200 to-gray-300', name: 'gris clair' },
+];
