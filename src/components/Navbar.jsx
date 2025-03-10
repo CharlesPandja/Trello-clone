3343c1befbe9ccfe0931d775e77790c26be7261a
CharlesPandja <pandjacharles@gmail.com>
2025-03-10 11:46:17 +0100

    Handling visibility of menu's items based on screen size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import menuImg from '../assets/menuLogo.png';
 import notifImg from '../assets/notifIcon.png';
 import helpImg from '../assets/helpIcon.png';
 import { NavLink } from 'react-router-dom';
 import ModalTableau from './ModalTableau';
 import { toggleOnOff } from '../store/sidebarOnOff';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Navbar = () => {
+    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
     const style = "w-auto h-10 px-2 flex justify-center items-center rounded-sm cursor-pointer hover:bg-stone-600";
     const styleText = "text-sm font-semibold text-stone-200";
 
     const modalRef = useRef()
 
+    const isOn = useSelector(state => state.onOff.isOn)
     const dispatch = useDispatch()
     const handleModalView = () => {
         if (modalRef.current) {
@@ -23,6 +25,16 @@ const Navbar = () => {
     const handleVisibilitySidebar = () => {
         dispatch(toggleOnOff());
     }
+
+    useEffect(() => {
+        const handleShowMenuItems = () => {
+            setScreenWidth(window.innerWidth)
+        }
+
+        window.addEventListener('resize', handleShowMenuItems)
+        return () => window.removeEventListener('resize', handleShowMenuItems)
+
+    }, [])
 
     return (
         <>
@@ -39,21 +51,25 @@ const Navbar = () => {
                     <div className={style}>
                         <p className={styleText}><NavLink to='/boards' >Espace de travail</NavLink></p>
                     </div>
-                    <div className={style}>
-                        <p className={styleText}>Récent</p>
-                    </div>
-                    <div className={style}>
-                        <p className={styleText}>Favoris</p>
-                    </div>
-                    <div className={style}>
-                        <p className={styleText}>Plus</p>
-                    </div>
+                    {screenWidth > 762 &&
+                        <><div className={style}>
+                            <p className={styleText}>Récent</p>
+                        </div>
+                            <div className={style}>
+                                <p className={styleText}>Favoris</p>
+                            </div>
+                            <div className={style}>
+                                <p className={styleText}>Plus</p>
+                            </div>
+                        </>}
                     <button onClick={handleModalView} className="w-10 h-10 text-black/80 bg-blue-400 text-3xl rounded-sm cursor-pointer hover:bg-blue-300">+</button>
                 </div>
                 <div className="flex justify-center items-center gap-2">
-                    <div className="flex justify-center items-center gap-6">
-                        <input className="w-full h-10 px-2 rounded-sm text-stone-200 text-sm border-1 border-stone-200 focus:border-green-300 outline-none" type="text" placeholder='Rechercher' />
-                    </div>
+                    {screenWidth > 762 &&
+                        <div className="flex justify-center items-center gap-6">
+                            <input className="w-full h-10 px-2 rounded-sm text-stone-200 text-sm border-1 border-stone-200 focus:border-green-300 outline-none" type="text" placeholder='Rechercher' />
+                        </div>
+                    }
                     <div className={style}>
                         <img className="max-w-5" src={notifImg} alt="menu image" />
                     </div>
